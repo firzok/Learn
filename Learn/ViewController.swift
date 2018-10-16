729c9e2ebbe3985bc3f584a5a57e2e3ad4f1ade1
Faateh Jarree <arronstn@gmail.com>
2018-10-16 22:14:31 +0500

    AR SUN ADDED 1.Warnings for constraints 2. Have misplaced some UI items for iPhone view

--- Learn/ViewController.swift
+++ Learn/ViewController.swift
@@ -19,11 +19,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        // OUR EDIT
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+
+        let sphere = SCNSphere(radius: 0.2)
+
+        let material = SCNMaterial()
+
+//        material.diffuse.contents = UIColor.red
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_sun.jpg")
+
+        sphere.materials = [material]
+
+        let node = SCNNode()
+
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+
+        node.geometry = sphere
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,7 +51,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         
-        let configuration = AROrientationTrackingConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
