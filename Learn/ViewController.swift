5ea4deb2aa77d18961efae408f3af0ceba0afbd1
Faateh Jarree <arronstn@gmail.com>
2018-10-17 00:35:28 +0500

    Text with SUN AR model implemented

--- Learn/ViewController.swift
+++ Learn/ViewController.swift
@@ -10,6 +10,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var textNode = SCNNode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,32 +21,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // OUR EDIT
-//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
 
         let sphere = SCNSphere(radius: 0.2)
 
         let material = SCNMaterial()
 
-//        material.diffuse.contents = UIColor.red
         material.diffuse.contents = UIImage(named: "art.scnassets/8k_sun.jpg")
 
         sphere.materials = [material]
 
         let node = SCNNode()
 
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        node.position = SCNVector3(x: 0, y: 0.5, z: -0.5)
 
         node.geometry = sphere
+        
         sceneView.scene.rootNode.addChildNode(node)
         
-        sceneView.autoenablesDefaultLighting = true
-        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        textNode.removeFromParentNode()
         
-        // Set the scene to the view
-//        sceneView.scene = scene
+        let textGeometry = SCNText(string: "SUN: 149.6 mil km away from Earth", extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.yellow
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(0, 0.1, -0.5)
+        
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+        
+        sceneView.autoenablesDefaultLighting = true
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -94,4 +105,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
 }
